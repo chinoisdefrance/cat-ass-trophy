711a00a89a1c6df357e63b345bf80a7b5d60d4c4
chinoisdefrance <justinelepuy@gmail.com>
2020-01-16 16:02:26 +0100

    amelioration poly blender et debut script tourelles

---
+++ cat_ass_trophy/Assets/script/tourelles.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tourelles : MonoBehaviour
+{
+
+    private Transform target;
+
+    [Header("Attributes")]
+
+    public float range = 15f;
+
+    
+
+    public float fireRate = 1f;
+    private float fireCountDown = 0f;
+
+    [Header("Unity Setup Fields")]
+
+    public string ennemyTag = "Enemy";
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] ennemis = GameObject.FindGameObjectsWithTag(ennemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in ennemis)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        if (fireCountDown <= 0f)
+        {
+            shoot();
+            fireCountDown = 1f / fireRate;
+        }
+
+        fireCountDown -= Time.deltaTime;
+    }
+
+    void shoot()
+    {
+        //Debug.Log("SHOOT !!!!");
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    void onDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ cat_ass_trophy/Assets/script/waveSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class waveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text waveCountDownText;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(spawnWave());
+            spawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountDownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator spawnWave ()
+    {
+        Debug.Log("CAT ASTROPHE !!!");
+
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            spawnEnnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+       
+    }
+
+    void spawnEnnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
